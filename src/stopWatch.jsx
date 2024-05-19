4e2069681afb75b170ccb347ebccd88073f59a7b
TNSHR <rohandw0@gmail.com>
2024-05-19 17:42:06 +0530

    orientation of time

--- src/stopWatch.jsx
+++ src/stopWatch.jsx
@@ -30,7 +30,7 @@ function StopWatch(){
         
         const minutes=Math.floor((time%3600)/60);
         const seconds = time%60;
-        // return `${minutes}:${seconds <10 ? '0':''}:${seconds}`;
+        return `${minutes}:${seconds <10 ? '0':''}${seconds}`;
     }
     return(
         <div>
