c10f059cee96324f4274efa54f4aa7cb83ba6f0f
TNSHR <rohandw0@gmail.com>
2024-05-20 08:23:10 +0530

    this code correction

--- src/stopWatch.jsx
+++ src/stopWatch.jsx
@@ -1,8 +1,8 @@
-import React,{useState,useEffect} from "react";
-function StopWatch(){
-    const [time,setTime]=useState(0);
-    const[isRunning,setIsRunning]=useState(false);
+import React,{useState, useEffect} from 'react';
 
+function StopWatch(){
+    const [time, setTime]=useState(0);
+    const [isRunning, setIsRunning]=useState(false);
     useEffect(()=>{
         let interval;
         if(isRunning){
@@ -10,40 +10,37 @@ function StopWatch(){
                 setTime(prevTime=>prevTime+1);
             },1000);
         }
-        else {
+        else{
             clearInterval(interval);
         }
-        return()=>clearInterval(interval);
+        return ()=>clearInterval(interval);
     },[isRunning]);
 
     const startStop=()=>{
-        setTime(0);
-        setIsRunning(true);
-    };
+        setIsRunning(!isRunning);};
+        const reset =()=>{
+            setTime(0);
+            setIsRunning(false);
+        };
+        const formatTime=(time)=>{
+            
+            const minutes = Math.floor((time%3600)/60);
+            const seconds=time%60;
+            return`${minutes}:${seconds <10 ? '0':''}${seconds}`;
+        }
+        return(
+            <div >
+                <h1>Stopwatch</h1>
+                <div style={{marginTop:"50px"}}>
+                    <div style={{fontSize:"40"}}> Time: {formatTime(time)}</div>
+                    <div style={{marginTop:"25px"}}>
+                        <button onClick={startStop}>{isRunning ?'Stop':'Start'}</button>
+                        <button onClick={reset}>Reset</button>
+                    </div>
 
-    const reset=()=>{
-       
-        setIsRunning(!isRunning);
-    };
-
-    const formatTime=(time)=>{
-        
-        const minutes=Math.floor(time/60);
-        const seconds = time%60;
-        return `${minutes}:${seconds <10 ? '0':''}${seconds}`;
-    }
-    return(
-        <div>
-            <h1>Stop Watch</h1>
-            <div>
-                <div>{formatTime(time)}</div>
-                <div>
-                    <button onClick={startStop}>{isRunning?'Stop':'Start'}</button>
-                    <button onClick={reset}>Reset</button>
                 </div>
-            </div>
 
-        </div>
-    )
-};
-export default StopWatch;
+            </div>
+        )
+    };
+    export default StopWatch;
